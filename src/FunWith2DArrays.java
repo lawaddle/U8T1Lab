1b0a2c0d92caca53641d991ed97b8251abb77c51
BT1E10_18 <sample@bths.edu>
2022-03-02 13:59:00 -0500

    shrug

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -1,6 +1,7 @@
 public class FunWith2DArrays {
     public static int totalElements (int[][] array2d)
     {
-        //return array2d.length * array2d[0].length;
+
+        return array2d.length * array2d[0].length;
     }
 }

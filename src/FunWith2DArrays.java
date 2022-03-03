d0bd854478813474d0dd6ee84229bf45c51d8294
Lawer <lawerthebest@gmail.com>
2022-03-02 19:38:54 -0500

    lab done i think

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -9,6 +9,7 @@ public class FunWith2DArrays {
         System.out.println(corned[0][corned[0].length-1]);
         System.out.println(corned[corned.length-1][0]);
         System.out.println(corned[corned.length-1][corned[0].length-1]);
+
     }
 
 }
